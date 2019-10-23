18268d8f5bc916072ec0ef763c1c2f9fcd2116d3
finn <isaaclindland@protonmail.com>
2019-10-23 10:17:56 -0600

    implemented partial google sign in

--- app/src/main/java/edu/cnm/deepdive/blackjack/BlackjackApplication.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/BlackjackApplication.java
@@ -4,12 +4,14 @@ import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.blackjack.service.BlackjackDatabase;
+import edu.cnm.deepdive.blackjack.service.GoogleSignInService;
 
 public class BlackjackApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    GoogleSignInService.setApplicationContext(this);
     Stetho.initializeWithDefaults(this);
     Picasso.setSingletonInstance(new Picasso.Builder(this).loggingEnabled(true).build());
     BlackjackDatabase.setApplicationContext(this);
